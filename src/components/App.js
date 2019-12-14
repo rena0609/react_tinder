022a83716d9c7d3b012805b11b65dbfe393251be
Rena <choco_rena0609@yahoo.co.jp>
2019-12-14 15:53:58 +0900

    button

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserDisplay from "./UserDisplay";
-import Button from "./Button";
+import {Button} from "./Button";
+import '../index.css';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <UserDisplay/>
+                <Button/>
             </div>
         );
     }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Button = () => {
+    return (
+        <div>
+            <button>Like</button>
+            <button>Nope</button>
+        </div>
+    );
+};
+

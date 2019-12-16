d12ad67371fbf8777bc8ba075d643a825e4899e6
Rena <choco_rena0609@yahoo.co.jp>
2019-12-16 20:50:17 +0900

    CSS

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
+                <h1 className="ui center aligned header">Tinder</h1>
                 <UserDisplay/>
             </div>
         );
